aa84c7231d864be907fe37997318c404d0492112
RSJ205 <rsuligaj@gmail.com>
2016-02-15 20:14:44 -0500

    Added a small settings screen with functional facebook logout button, can be linked to from settings button in home screen

--- app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/HomeScreenActivity.java
+++ app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/HomeScreenActivity.java
@@ -90,6 +90,8 @@ public class HomeScreenActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
