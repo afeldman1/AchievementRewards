06510c871ae974c7058103471aecf65e8d7720c8
Adam Feldman <afeldman1@gmail.com>
2016-02-29 20:05:13 -0500

    Added support for displaying achieved Achievements on Home Screen

--- app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/HomeActivity.java
+++ app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/HomeActivity.java
@@ -1,15 +1,23 @@
 package com.applications.achievementRewards.achievementRewardsAndroid;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.applications.achievementRewards.achievementRewardsAndroid.databaseTasks.UserAchievedAchievements_DatabaseTask;
 import com.applications.achievementRewards.achievementRewardsAndroid.objects.Adapter_OnWayRewards;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-    String[] avail_rewards = {"availReward1", "availReward2"};
+    //String[] avail_rewards = {"availReward1", "availReward2"};
     String[] on_way_rewards = {"reward1", "reward2"};
     //String[] secondStringArray = {"Here", "Is", "My", "second", "layout", "woo!"};
 
@@ -21,8 +29,15 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        new UserAchievedAchievements_DatabaseTask().execute(sharedPreferences.getLong("currUserID", 0));
+
+    }
+
+    @Subscribe
+    public void onEvent(List<String> userAchievedAchievementsModels){
         ArrayAdapter<String> myAdapter=new
-                ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, avail_rewards);
+                ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, userAchievedAchievementsModels);
         ListView myList=(ListView)
                 findViewById(R.id.avail_list);
         myList.setAdapter(myAdapter);
@@ -30,6 +45,17 @@ public class HomeActivity extends AppCompatActivity {
 
         ListView myList2 = (ListView) findViewById(R.id.on_way_list);
         myList2.setAdapter(new Adapter_OnWayRewards(this,on_way_rewards));
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        EventBus.getDefault().unregister(this);
+        super.onStop();
     }
 }
