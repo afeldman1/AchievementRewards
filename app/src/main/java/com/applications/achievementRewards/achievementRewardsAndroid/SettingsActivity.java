c6b0f503bdd3de7c99992b164acb3ec586a1fc59
RSJ205 <rsuligaj@gmail.com>
2016-03-08 03:50:03 -0500

    Complete Functionality of all predetermined options added. Included requests for location access in the manifests.xml, Changed one switch to reference gps location, and another to reference network locations.  Switches set themselves with respect to devices state, and allow user to enter location settings page.

--- app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/SettingsActivity.java
+++ app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/SettingsActivity.java
@@ -1,7 +1,15 @@
 package com.applications.achievementRewards.achievementRewardsAndroid;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.location.*;
+import android.content.Context;
+import android.view.View;
+import android.widget.Switch;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,8 +17,94 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
 
-        
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        LocationManager lm = null;
+        boolean gps_enabled=false,network_enabled=false;
+        if(lm==null)
+            lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try{
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }catch(Exception ex){}
+        try{
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        }catch(Exception ex){}
+
+        final Switch switch_coarse = (Switch)findViewById(R.id.switch1);
+        final Switch switch_fine = (Switch)findViewById(R.id.switch2);
+        switch_fine.setChecked(gps_enabled);
+        switch_coarse.setChecked(network_enabled);
+
+        final boolean gps_final = gps_enabled;
+        final boolean network_final = network_enabled;
+
+        switch_fine.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(SettingsActivity.this);
+                if (switch_fine.isChecked()) {
+                    dialog.setMessage(R.string.gps_not_enabled);
+                } else {
+                    dialog.setMessage(R.string.gps_enabled);
+                }
+                dialog.setPositiveButton(R.string.open_location_settings, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                        //get gps
+                    }
+                });
+                dialog.setNegativeButton(R.string.Cancel, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        switch_fine.setChecked(gps_final);
+                    }
+                });
+                dialog.show();
+                switch_fine.setChecked(gps_final);
+            }
+        });
+
+        switch_coarse.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(SettingsActivity.this);
+                if (switch_coarse.isChecked()) {
+                    dialog.setMessage(R.string.network_not_enabled);
+                } else {
+                    dialog.setMessage(R.string.network_enabled);
+                }
+                dialog.setPositiveButton(R.string.open_location_settings, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                        //get gps
+                    }
+                });
+                dialog.setNegativeButton(R.string.Cancel, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        switch_coarse.setChecked(gps_final);
+                    }
+                });
+                dialog.show();
+                switch_coarse.setChecked(gps_final);
+            }
+        });
 
     }
+
+
 }
