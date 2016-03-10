ef164946b5eda3be90617034a8d7838258495aa6
RSJ205 <rsuligaj@gmail.com>
2016-03-10 02:53:15 -0500

    Complete Functionality of all predetermined options added. Included requests for location access in the manifests.xml, Changed one switch to reference gps location, and another to reference network locations.  Switches set themselves with respect to devices state, and allow user to enter location settings page.  Added settings button back into home activity

--- app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/SignInActivity.java
+++ app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.applications.achievementRewards.achievementRewardsAndroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -32,6 +33,8 @@ public class SignInActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
