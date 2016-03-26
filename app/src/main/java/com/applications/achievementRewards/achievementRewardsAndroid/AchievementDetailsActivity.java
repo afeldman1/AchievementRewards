18e278e8e0351e0e01207f3d87d179800b012b33
RSJ205 <rsuligaj@gmail.com>
2016-03-26 16:41:46 -0400

    -Full merge and inclusion of test button to all screens.

--- app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/AchievementDetailsActivity.java
+++ app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/AchievementDetailsActivity.java
@@ -3,6 +3,8 @@ package com.applications.achievementRewards.achievementRewardsAndroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -88,5 +90,34 @@ public class AchievementDetailsActivity extends AppCompatActivity {
     public void onStop() {
         EventBus.getDefault().unregister(this);
         super.onStop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_test) {
+            Intent intent = new Intent(this, DisplayLocation.class);
+            startActivity(intent);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/MerchantDetailsActivity.java
+++ app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/MerchantDetailsActivity.java
@@ -3,6 +3,8 @@ package com.applications.achievementRewards.achievementRewardsAndroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,7 +12,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.applications.achievementRewards.achievementRewardsAndroid.databaseTasks.MerchantDetails_DatabaseTask;
-import com.applications.achievementRewards.achievementRewardsAndroid.databaseTasks.Merchants_DatabaseTask;
+//import com.applications.achievementRewards.achievementRewardsAndroid.databaseTasks.Merchants_DatabaseTask;
 import com.applications.achievementRewards.achievementRewardsAndroid.objects.MerchantModel;
 import com.applications.achievementRewards.achievementRewardsAndroid.objects.UserAchievementModel;
 
@@ -74,4 +76,34 @@ public class MerchantDetailsActivity extends AppCompatActivity {
         EventBus.getDefault().unregister(this);
         super.onStop();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_test) {
+            Intent intent = new Intent(this, DisplayLocation.class);
+            startActivity(intent);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
