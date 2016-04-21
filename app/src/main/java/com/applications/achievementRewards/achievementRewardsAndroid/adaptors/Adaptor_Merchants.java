a4783cfa8a246d34d94d949617701070a5929d77
Adam Feldman <afeldman1@gmail.com>
2016-04-21 04:07:17 -0400

    Implementing searching capability in All Merchants screen

--- app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/adaptors/Adaptor_Merchants.java
+++ app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/adaptors/Adaptor_Merchants.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,18 +14,20 @@ import com.applications.achievementRewards.achievementRewardsAndroid.R;
 import com.applications.achievementRewards.achievementRewardsAndroid.objects.MerchantModel;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Adam Feldman on 4/9/2016.
  */
-public class Adaptor_Merchants extends BaseAdapter {
+public class Adaptor_Merchants extends BaseAdapter implements Filterable {
     Context context;
-    List<MerchantModel> merchantModels;
+    List<MerchantModel> merchantModelsUnfiltered, merchantModels;
     private static LayoutInflater inflater = null;
 
     public Adaptor_Merchants(Context context, List<MerchantModel> merchantModels) {
         this.context = context;
+        this.merchantModelsUnfiltered = merchantModels;
         this.merchantModels = merchantModels;
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
@@ -46,7 +50,7 @@ public class Adaptor_Merchants extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
-            convertView = inflater.inflate(R.layout.composite_merchants, null);
+            convertView = inflater.inflate(R.layout.composite_merchants, parent, false);
         }
 
         MerchantModel merchantModel = (MerchantModel) getItem(position);
@@ -58,5 +62,41 @@ public class Adaptor_Merchants extends BaseAdapter {
         merchant_tv.setText(merchantModel.getMerchantName());
 
         return convertView;
+    }
+
+    @SuppressWarnings("redundant")
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+
+                List<MerchantModel> filteredMerchantModels = new ArrayList<>();
+                constraint = constraint.toString().toLowerCase();
+                merchantModels = merchantModelsUnfiltered;
+
+                for (MerchantModel merchantModel : merchantModels) {
+                    if (merchantModel.getMerchantName().toLowerCase().contains(constraint) ||
+                            (merchantModel.getMerchantDescription() != null && merchantModel.getMerchantDescription().toLowerCase().contains(constraint))) {
+                        filteredMerchantModels.add(merchantModel);
+                    }
+                }
+
+                results.count = filteredMerchantModels.size();
+                results.values = filteredMerchantModels;
+
+                return results;
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                merchantModels = (List<MerchantModel>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+
+        return filter;
     }
 }
