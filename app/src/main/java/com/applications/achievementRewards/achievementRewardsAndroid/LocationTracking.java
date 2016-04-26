ca04f8d71289ab9d77c2719519d193adfd913f03
RSJ205 <rsuligaj@gmail.com>
2016-04-25 22:01:10 -0400

    Added code to update location every 10 updates and set that in the UserAchievementObject. May still need to call a database task to push this to server.

--- app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/LocationTracking.java
+++ app/src/main/java/com/applications/achievementRewards/achievementRewardsAndroid/LocationTracking.java
@@ -2,12 +2,18 @@ package com.applications.achievementRewards.achievementRewardsAndroid;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.applications.achievementRewards.achievementRewardsAndroid.adaptors.Adapter_OnWayRewards;
+import com.applications.achievementRewards.achievementRewardsAndroid.databaseTasks.UserAchievements_DatabaseTask;
+import com.applications.achievementRewards.achievementRewardsAndroid.objects.UserAchievementModel;
+import com.applications.achievementRewards.achievementRewardsAndroid.objects.wrapper.UserAchievementModels;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.drive.Permission;
@@ -16,8 +22,12 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 
+import org.greenrobot.eventbus.Subscribe;
+
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class LocationTracking extends Service implements
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
@@ -26,14 +36,17 @@ public class LocationTracking extends Service implements
     Location mLastLocation= null;
     Location mCurrentLocation = null;
 
-    int total_distance = 0;
+    double total_distance = 0;
     String mLastUpdateTime = null;
+    int total_count = 0;
 
     LocationRequest mLocationRequest = null;
 
+    UserAchievementModels userAchievementModels = new UserAchievementModels();
+
     protected void createLocationRequest() {
         mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(5000);
+        mLocationRequest.setInterval(60000);
         mLocationRequest.setFastestInterval(5000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
@@ -66,11 +79,21 @@ public class LocationTracking extends Service implements
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
                 .addLocationRequest(mLocationRequest);
 
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        new UserAchievements_DatabaseTask().execute(sharedPreferences.getLong("currUserID", 0));
+
         super.onCreate();
+    }
+
+    @Subscribe
+    public void onDataLoadEvent(UserAchievementModels userAchievementModelsGet) {
+        userAchievementModels = userAchievementModelsGet;
     }
 
     @Override
     public void onDestroy() {
+
+
         stopLocationUpdates();
         mGoogleApiClient.disconnect();
         super.onDestroy();
@@ -111,8 +134,29 @@ public class LocationTracking extends Service implements
         if (mLastLocation != null) {
             total_distance += mCurrentLocation.distanceTo(mLastLocation); //Distance in meters
             //TOTAL DISTANCE is essentially all the distance gathered in this session. Preferably in "ON DESTROY"
-            //
+            total_count ++;
         }
 
+        if (total_count >= 10) {
+            total_count = 0;
+
+            total_distance = total_distance * 0.000621371; //CONVERSION FROM METERS TO MILES.
+
+            for (UserAchievementModel userAchievementModel : userAchievementModels) {
+                if (userAchievementModel.getProgress() < userAchievementModel.getTrackingMax())
+                {
+                    double curProg = userAchievementModel.getProgress();
+                    curProg += total_distance;
+                    if (curProg >= userAchievementModel.getTrackingMax()) {
+                        curProg = userAchievementModel.getTrackingMax();
+                    }
+
+                    userAchievementModel.setProgress(curProg);
+
+                }
+            }
+            total_distance = 0;
+
+        }
     }
 }
